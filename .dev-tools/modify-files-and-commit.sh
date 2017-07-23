ea59c909fda9d3d12bce79ee13df1eea516edb88
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-23 11:51:25 +0200

    Change GH_TOKEN to GOOGLESPAM_TOKEN / Test - Re-Gen

--- .dev-tools/modify-files-and-commit.sh
+++ .dev-tools/modify-files-and-commit.sh
@@ -44,7 +44,7 @@ git remote rm origin
 # Add Remote with Secure Key
 # **************************
 
-git remote add origin https://${GH_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
+git remote add origin https://${GOOGLESPAM_TOKEN}@github.com/${TRAVIS_REPO_SLUG}.git
 
 # **********************************************************************************
 # List Remotes ONLY DURING testing - do not do this on live repo / possible key leak
