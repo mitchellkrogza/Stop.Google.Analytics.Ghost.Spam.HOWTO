2da189f57a9beb058eabacdb63d0b16f80ec68b6
Mitchell Krog <mitchellkrog@gmail.com>
2017-07-24 11:47:55 +0200

    Test modified get page titles script

--- .dev-tools/get-page-titles.sh
+++ .dev-tools/get-page-titles.sh
@@ -14,6 +14,7 @@ _output=$TRAVIS_BUILD_DIR/.dev-tools/_output_source/page-titles.list
 while IFS= read -r LINE
 do
 #printf '\t"~*%s%s%s"\t\t%s\n' "\b" "${LINE}" "\b" "$_action1" >> "$_tmpnginx1"
-wget -qO- '${LINE}' |   perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' >> $_output
+#wget -qO- '${LINE}' |   perl -l -0777 -ne 'print $1 if /<title.*?>\s*(.*?)\s*<\/title/si' >> $_output
+curl -s ${LINE} | grep -o "<title>[^<]*" | cut -d'>' -f2-  >> $_output
 done < $_input
 
