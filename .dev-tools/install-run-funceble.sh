14a6904f3b1dbf38f34b17c2130ca36022d12c96
Mitchell Krog <mitchellkrog@gmail.com>
2017-08-31 15:37:32 +0200

    Fix Execution of Funceble.

--- .dev-tools/install-run-funceble.sh
+++ .dev-tools/install-run-funceble.sh
@@ -34,12 +34,13 @@ cd $TRAVIS_BUILD_DIR/.dev-tools/_funceble/
 
 YEAR=$(date +%Y)
 MONTH=$(date +%m)
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool -u --dev --autosave-minutes 40 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
+
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/tool --dev -u --autosave-minutes 40 --commit-autosave-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER} [funceble]" --commit-results-message "V1.${YEAR}.${MONTH}.${TRAVIS_BUILD_NUMBER}" -i
 
 # ************************************
 #  Run Funceble and Check Domains List
 # ************************************
 
-sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble --travis -a -ex -h --plain --split -f $_input
+sudo bash $TRAVIS_BUILD_DIR/.dev-tools/_funceble/funceble --cmd-before-end "bash $TRAVIS_BUILD_DIR/.dev-tools/modify-readme.sh && php $TRAVIS_BUILD_DIR/.dev-tools/generate-google-exclude.php" --travis -a -ex -h --plain --split -f $_input
 
 exit 0
