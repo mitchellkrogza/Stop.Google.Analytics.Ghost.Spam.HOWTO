b4b17c36b9233ddfd4b842b1226cd8ecc1255c0d
Mitchell Krog <mitchellkrog@gmail.com>
2021-07-01 16:08:15 +0200

    Update run-pyfunceble.sh

--- .dev-tools/run-pyfunceble.sh
+++ .dev-tools/run-pyfunceble.sh
@@ -31,7 +31,7 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    PyFunceble --ci -dbr 5 -ex --dns 1.1.1.1 1.0.0.1 --ci-end-command "bash ${TRAVIS_BUILD_DIR}/.dev-tools/final-commit.sh" --plain -–ci-max-minutes 20 --ci-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --ci-end-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}" -f ${_input}
+    PyFunceble --ci --plain -dbr 5 -ex --dns 1.1.1.1 1.0.0.1 --ci-max-minutes 20 --ci-end-command "bash ${TRAVIS_BUILD_DIR}/.dev-tools/final-commit.sh" --ci-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --ci-end-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}" -f ${_input}
 
 }
 
