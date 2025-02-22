61609c86f9d5de9ab709b6ac4999b6595c45e560
funilrys <contact@funilrys.com>
2025-02-22 19:06:47 +0100

    Update run-pyfunceble.sh.

--- dev-tools/run-pyfunceble.sh
+++ dev-tools/run-pyfunceble.sh
@@ -30,7 +30,7 @@ RunFunceble () {
         rm "${pyfuncebleProductionConfigurationFileLocation}"
     fi
 
-    PyFunceble --ci --plain -dbr 5 -ex --dns 1.1.1.1 1.0.0.1 --ci-max-minutes 20 --ci-end-command "bash ./dev-tools/final-commit.sh" --ci-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --ci-end-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}" -f ${_input}
+    PyFunceble --ci --ci-branch dev --ci-distribution-branch --plain -w 10 -dbr 5 -ex --dns 1.1.1.1 1.0.0.1 --ci-max-minutes 20 --ci-end-command "bash ./dev-tools/final-commit.sh" --ci-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER} [PyFunceble]" --ci-end-commit-message "V1.${yeartag}.${monthtag}.${TRAVIS_BUILD_NUMBER}" -f ${_input}
 
 }
 
